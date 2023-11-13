d84e0f3867262567533f44d4002c8885986d791c
lordeadroid <rishabhrawat34@gmail.com>
2023-11-13 18:47:06 +0530

    Feature: Added nav-bar elements

--- src/NavigationBar.tsx
+++ src/NavigationBar.tsx
@@ -1,7 +1,40 @@
+const LogoName = () => {
+  return <h1 className="logo">Kart Cart</h1>;
+};
+
+const SearchBar = () => {
+  return (
+    <div>
+      <input type="search" name="searchbar" className="search-bar" />
+    </div>
+  );
+};
+
+const Account = () => {
+  return (
+    <div className="account">
+      <p>Account</p>
+    </div>
+  );
+};
+
+const Cart = () => {
+  return (
+    <div className="cart">
+      <p>Cart</p>
+    </div>
+  );
+};
+
 const NavigationBar = () => {
-  return <div className="navigation-bar">
-    <h1>Kart Cart</h1>
-  </div>
+  return (
+    <div className="navigation-bar">
+      <LogoName />
+      <SearchBar />
+      <Account />
+      <Cart />
+    </div>
+  );
 };
 
 export default NavigationBar;
