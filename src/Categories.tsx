b76294cd1f5ae4fbc4099ca71e75944b54b8110a
lordeadroid <rishabhrawat34@gmail.com>
2023-12-15 11:45:28 +0530

    Created a general component for category products

--- src/Categories.tsx
+++ src/Categories.tsx
@@ -1,9 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { URL } from './constants';
+
 interface Cat {
   category: string;
 }
 
 const Categories = (props: Cat): React.JSX.Element => {
-  return <p>category && {props.category}</p>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(`${URL.category}/men`)
+      .then((res) => res.json())
+      .then((resData) => {
+        setData(resData);
+      });
+  }, []);
+
+  const Card = (props: any) => {
+    return <p>props</p>;
+  };
+
+  const page: React.JSX.Element[] = data.map((element, index) => {
+    return <Card key={index} data={element} />;
+  });
+
+  return <>{page}</>;
 };
 
 export default Categories;
---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+import { urlProps } from './interfaces';
+
+const serverURL: string = 'https://kart-cart-bff.onrender.com';
+export const URL: urlProps = {
+  category: `${serverURL}/category`,
+};
