fe16dd79187f6780adef2832ae65e16b7cccea9f
lordeadroid <rishabhrawat34@gmail.com>
2023-12-16 15:54:57 +0530

    Changed images when hovered

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import { CategoryData } from '../utils/interfaces';
 
 const Card = ({ data, productId }: CategoryData) => {
+  const [isHovered, setIsHovered] = useState(false);
   const [foreImage, backImage] = data.images;
+
+  const image: string = isHovered ? backImage : foreImage;
+
+  const handleHover = (): void => {
+    setIsHovered(!isHovered);
+  };
+
   return (
     <div className="card">
-      <div className="image">
-        <img src={foreImage} alt={data.name} />
+      <div
+        className="image"
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHover}
+      >
+        <img src={image} alt={data.name} />
       </div>
       <div className="name">{data.name}</div>
       <div className="prices">
