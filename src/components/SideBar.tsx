864f89c5022a6941306b90ac3b31cf3e8c22578d
lordeadroid <rishabhrawat34@gmail.com>
2023-12-17 18:39:15 +0530

    added stylex

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,16 @@
+import * as stylex from '@stylexjs/stylex';
+
+const styles = stylex.create({
+  base: {
+    fontSize: 16,
+    lineHeight: 1.5,
+    color: 'rgb(60,60,60)',
+  },
+  highlighted: {
+    color: 'rebeccapurple',
+  },
+});
+
 export const Sidebar = (): React.JSX.Element => {
   return (
     <div className="sidebar">
