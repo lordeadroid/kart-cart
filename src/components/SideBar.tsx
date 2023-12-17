5ba9e14f891b632f6b1744dd43d3c0e019579398
lordeadroid <rishabhrawat34@gmail.com>
2023-12-17 14:38:59 +0530

    Chore: Added stylex lib in project

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,8 @@
 export const Sidebar = (): React.JSX.Element => {
-  return <div className="sidebar">
-    <input type="radio" name="Low To High" id="" />
-    <input type="radio" name="High To Low" id="" />
-  </div>
+  return (
+    <div className="sidebar">
+      <input type="radio" name="Low To High" id="" />
+      <input type="radio" name="High To Low" id="" />
+    </div>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import inject from '@stylexjs/dev-runtime';
+
+inject({
+  classNamePrefix: 'x',
+  dev: true,
+  test: false,
+  useRemForFontSize: false,
+  runtimeInjection: false,
+  styleResolution: 'application-order'
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
