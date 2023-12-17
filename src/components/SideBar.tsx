ba613fccddb148fd7ede494ebff9daaf58ece86b
lordeadroid <rishabhrawat34@gmail.com>
2023-12-17 12:48:03 +0530

    Added sidebar file

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,6 @@
+export const Sidebar = (): React.JSX.Element => {
+  return <div className="sidebar">
+    <input type="radio" name="Low To High" id="" />
+    <input type="radio" name="High To Low" id="" />
+  </div>
+};
