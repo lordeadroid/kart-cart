f73c8eeecc79c2c8652e10475f319d54ecc679ef
lordeadroid <rishabhrawat34@gmail.com>
2023-12-18 13:29:28 +0530

    Created dropdown menu for sorting

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,20 @@
+import { ChangeEvent } from 'react';
+
 export const Sidebar = (): React.JSX.Element => {
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>): void => {
+    console.log(event.target.value);
+  };
+
   return (
-    <div className="sidebar">
-      <input type="radio" name="Low To High" id="" />
-      <input type="radio" name="High To Low" id="" />
+    <div className="w-1/4 flex flex-col h-fit pl-10 text-xl">
+      <label htmlFor="sort-button" className="text-3xl mb-2">
+        Sort
+      </label>
+      <select className="w-60" onChange={handleChange}>
+        <option value="">--</option>
+        <option value="asc">Low To High</option>
+        <option value="des">High To Low</option>
+      </select>
     </div>
   );
 };
