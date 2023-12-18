be55d6f11f40fba9fa7b179e0df31d662099ac8f
lordeadroid <rishabhrawat34@gmail.com>
2023-12-18 15:32:24 +0530

    Fix issue of image not changing on homepage

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,33 +1,38 @@
 import React, { useEffect, useState } from 'react';
 
 const ImageGallery = (): React.JSX.Element => {
-  const [imageIndex, setImageIndex] = useState(0);
+  const [imageIndex, setImageIndex] = useState<number>(0);
   const [images, setImages] = useState<string[]>([
     'https://miro.medium.com/v2/resize:fit:640/format:webp/1*9EBHIOzhE1XfMYoKz1JcsQ.gif',
   ]);
 
-  useEffect(() => {
+  useEffect((): void => {
     fetch('https://kart-cart-bff.onrender.com/homepagedata')
       .then((response) => response.json())
       .then((data: string[]): void => {
         setImages(data);
-      })
-      .catch((error) => {
-        console.error(error);
       });
+  }, []);
 
+  useEffect((): (() => void) => {
     const intervalId = setInterval(() => {
       setImageIndex(imageIndex === images.length - 1 ? 0 : imageIndex + 1);
-    }, 2500);
+    }, 2000);
 
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  });
 
   return (
-    <div>
-      <img src={images[imageIndex]} alt="Images of products" />
+    <div className="flex justify-between">
+      <div className="h-10 w-5 border-2"></div>
+      <img
+        src={images[imageIndex]}
+        alt="Images of products"
+        className="h-[900px]"
+      />
+      <div className="h-10 w-5 border-2"></div>
     </div>
   );
 };
