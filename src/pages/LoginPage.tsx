93b7702b6d8a49257df3be4315923a6772b0c12a
lordeadroid <rishabhrawat34@gmail.com>
2024-01-06 20:54:08 +0530

    centered login page input box

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -28,23 +28,31 @@ const LoginPage = (): React.JSX.Element => {
   }
 
   const Login: React.JSX.Element = (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Username</label>
-      <input
-        type="username"
-        name="Email"
-        placeholder="Username"
-        onChange={(event) => setUsername(event.target.value)}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      <input type="submit" value="Submit" />
-    </form>
+    <div className="flex flex-col text-center text-xl">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Username</label>
+          <input
+            type="username"
+            name="Email"
+            placeholder="Username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+    </div>
   );
 
   return Login;
